6c9767023003b65df6cc221edab4a6c2798bebf5
Julian Nicholls <juliannicholls29@gmail.com>
2019-04-26 19:36:53 +0100

    Improve sorting of roads.

--- load-xml.js
+++ load-xml.js
@@ -2,8 +2,17 @@ const fs = require('fs');
 const express = require('express');
 const parser = require('fast-xml-parser');
 
+const replacements = {
+  Junction: /jct/i,
+  Junction: /jct jct/i,
+  Junction: /jn/i,
+  '\n': /&#xD;&#xA;/,
+  '\n': /&#xD;/,
+  '\n': /&#xA;/,
+};
+
 function flattenRoadworks(jsonData) {
-  return jsonData.Report.HE_PLANNED_ROADWORKS.HE_PLANNED_WORKS_Collection.HE_PLANNED_WORKS.reduce(
+  const rawData = jsonData.Report.HE_PLANNED_ROADWORKS.HE_PLANNED_WORKS_Collection.HE_PLANNED_WORKS.reduce(
     (works, cur) => {
       const eastNorth =
         cur.EASTNORTH.Report.EASTINGNORTHING.EASTNORTH_Collection.EASTNORTH;
@@ -31,6 +40,33 @@ function flattenRoadworks(jsonData) {
     },
     []
   );
+
+  // Sort the roadworks:
+  //  First, Motorways: M1, M2, ... M80, ...
+  //  Then, A roads: A1, A2, ... A337, ...
+
+  const numRegex = /^[AM](\d{1,4})\s?([AM])?(\d{1,4})?/;
+
+  rawData.sort((a, b) => {
+    if (a.roads[0] !== b.roads[0]) return a.roads[0] < b.roads[0] ? 1 : -1;
+
+    const left = a.roads.match(numRegex);
+    const right = b.roads.match(numRegex);
+
+    if (left[1] !== right[1]) return Number(left[1]) - Number(right[1]);
+
+    if (left[2] === undefined) {
+      if (right[2] === undefined) return 0;
+
+      return -1;
+    } else if (right[2] === undefined) return 1;
+
+    if (left[2] !== right[2]) return left[2] < right[2] ? 1 : -1;
+
+    return Number(left[3]) - Number(right[3]);
+  });
+
+  return rawData;
 }
 
 const app = express();
