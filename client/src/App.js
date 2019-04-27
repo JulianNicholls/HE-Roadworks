113116c2157644d23a80e7aa6bef905672c8cb3c
Julian Nicholls <juliannicholls29@gmail.com>
2019-04-27 23:25:23 +0100

    Make Header and re-word info panel.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import Header from './components/Header';
 import SelectionPanel from './components/SelectionPanel';
 import RoadworksList from './components/RoadworksList';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <main className="container">
-        <h1 className="centred">Roadworks</h1>
         <div className="top-section">
           <div className="introduction">
             <p>
@@ -16,9 +17,8 @@ function App() {
                 href="https://www.gov.uk/government/organisations/department-for-transport"
                 title="DfT Site"
               >
-                {' '}
-                Department for Transport (DfT){' '}
-              </a>
+                Department for Transport (DfT)
+              </a>{' '}
               began to issue datasets for public use under the{' '}
               <a
                 href="https://www.nationalarchives.gov.uk/doc/open%2Dgovernment%2Dlicence/"
@@ -32,17 +32,20 @@ function App() {
 
             <p>
               Presented here is the latest data on planned roadworks on Highways
-              England's roads, distilled from the data files on{' '}
+              England's roads, distilled from the latest data file on{' '}
               <a
                 href="https://data.gov.uk/dataset/5b3267d8-4307-4eef-a9af-3a4c28224694/planned-road-works-on-the-he-road-network"
                 title="dataset"
               >
                 this&nbsp;page
               </a>
-              . You can find information by road number, or search by location.
-              The roadworks that are shown have already started or will start
-              within the next week. Roadworks which should have been complete at
-              least a week ago are filtered out (hopeful, I know).
+              .
+            </p>
+            <p>
+              You can find information by road number, or search by location. The
+              roadworks that are shown have already started or will start within
+              the next week. Roadworks which should have been complete at least a
+              week ago are filtered out (hopeful, I know).
             </p>
           </div>
           <SelectionPanel />
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Roadworks</h1>
+    </header>
+  );
+};
+
+export default Header;
