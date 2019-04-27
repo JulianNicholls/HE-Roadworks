a869e3b4c06ccf04e09aa17a205fe8f08840c01b
Julian Nicholls <juliannicholls29@gmail.com>
2019-04-27 21:54:53 +0100

    Add location searching

--- client/src/components/SelectionPanel.js
+++ client/src/components/SelectionPanel.js
@@ -1,24 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
-import WorksContext from '../Context';
+import WorksContext from '../context';
 
 const SelectionPanel = () => {
-  const { roads, setSelected } = useContext(WorksContext);
+  const { roads, selected, setSelected, setSearchText } = useContext(
+    WorksContext
+  );
+  const [location, setLocation] = useState('');
 
   const changeRoad = e => {
     setSelected(e.target.value);
+    setLocation('');
+    setSearchText('');
+  };
+
+  const newLocation = e => {
+    const newLoc = e.target.value;
+
+    setLocation(newLoc);
+
+    if (newLoc.length > 2) {
+      setSelected('');
+      setSearchText(newLoc);
+    }
   };
 
   return (
     <div className="selection-panel">
-      <label htmlFor="road">Road</label>
-      <select id="road" onChange={changeRoad}>
-        {roads.map(({ roads }, index) => (
-          <option key={index} value={roads}>
-            {roads}
-          </option>
-        ))}
-      </select>
+      <div>
+        <label htmlFor="road">Road</label>
+        <select id="road" onChange={changeRoad} defaultValue={selected}>
+          {roads.map(({ roads }, index) => (
+            <option key={index} value={roads}>
+              {roads}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="location">Location</label>
+        <input
+          type="text"
+          id="location"
+          value={location}
+          onChange={newLocation}
+        />
+      </div>
     </div>
   );
 };
