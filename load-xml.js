a0aeb71f0c1f19b4f7e7e982f637b090df788989
Julian Nicholls <juliannicholls29@gmail.com>
2019-04-28 21:13:10 +0100

    Add serve option to load-xml

--- load-xml.js
+++ load-xml.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
-const express = require('express');
 const parser = require('fast-xml-parser');
 
-const correctTypos = description => {
+const cleanup = description => {
   const replacements = [
     { regex: /jct jct/gi, replacement: 'Junction' },
     { regex: /jct/gi, replacement: 'Junction' },
@@ -31,6 +30,39 @@ const correctTypos = description => {
   }, description);
 };
 
+// The wrinkle if the sorting is that there are entries like 'M27 M271' which
+// would need to be sorted between 'M27' and 'M27 M3'. There are actually entries
+// that have more than two roads, but they tend to be more or less unique.
+
+// Regex for splitting, thus:
+//  'M27 M3' -> [matchdata, '27', 'M', '3']
+const numRegex = /^[AM](\d{1,4})\s?([AM])?(\d{1,4})?/;
+
+const compare = (a, b) => {
+  // First of all, primary road M-ways before A-roads
+  if (a.roads[0] !== b.roads[0]) return a.roads[0] < b.roads[0] ? 1 : -1;
+
+  // Both primaries are M-way or A-road.
+  const left = a.roads.match(numRegex);
+  const right = b.roads.match(numRegex);
+
+  // If the road number differs, just return the difference.
+  if (left[1] !== right[1]) return Number(left[1]) - Number(right[1]);
+
+  if (left[2] === undefined) {
+    // No second road left...
+    if (right[2] === undefined) return 0; // No second road right means they are equal.
+
+    return -1; // Left no second, right has, left is less
+  } else if (right[2] === undefined) return 1; // Left has second, right not, left is more
+
+  // Back to A-road vs M-way on secondary
+  if (left[2] !== right[2]) return left[2] < right[2] ? 1 : -1;
+
+  // Both secondary are A-road or M-way, return difference
+  return Number(left[3]) - Number(right[3]);
+};
+
 function flattenRoadworks(jsonData) {
   const rawData = jsonData.Report.HE_PLANNED_ROADWORKS.HE_PLANNED_WORKS_Collection.HE_PLANNED_WORKS.reduce(
     (works, cur) => {
@@ -43,7 +75,7 @@ function flattenRoadworks(jsonData) {
         roads.ROAD_NUMBER ||
         roads.map(({ ROAD_NUMBER }) => ROAD_NUMBER).join(' ');
 
-      const description = correctTypos(cur.DESCRIPTION);
+      const description = cleanup(cur.DESCRIPTION);
 
       const item = {
         startDate: new Date(cur.SDATE),
@@ -64,39 +96,25 @@ function flattenRoadworks(jsonData) {
   );
 
   // Sort the roadworks:
-  //  First, Motorways: M1, M2, ... M80, ...
-  //  Then, A roads: A1, A2, ... A337, ...
+  //  First, the Motorways in numerical order
+  //  Then, the A roads, likewise in numerical order
 
-  const numRegex = /^[AM](\d{1,4})\s?([AM])?(\d{1,4})?/;
-
-  rawData.sort((a, b) => {
-    if (a.roads[0] !== b.roads[0]) return a.roads[0] < b.roads[0] ? 1 : -1;
-
-    const left = a.roads.match(numRegex);
-    const right = b.roads.match(numRegex);
-
-    if (left[1] !== right[1]) return Number(left[1]) - Number(right[1]);
-
-    if (left[2] === undefined) {
-      if (right[2] === undefined) return 0;
-
-      return -1;
-    } else if (right[2] === undefined) return 1;
-
-    if (left[2] !== right[2]) return left[2] < right[2] ? 1 : -1;
-
-    return Number(left[3]) - Number(right[3]);
-  });
+  rawData.sort(compare);
 
   return rawData;
 }
 
-const app = express();
+let serving = false;
+let filename = process.argv[2]; // Assume not serving
+let app;
 
-const xmlData = fs.readFileSync(
-  process.argv[2] || 'he_roadworks_2019_04_15.xml',
-  'utf-8'
-);
+if (process.argv[2].toLowerCase() === 'serve') {
+  serving = true;
+  filename = process.argv[3];
+  app = require('express')();
+}
+
+const xmlData = fs.readFileSync(filename, 'utf-8');
 
 const parserOptions = {
   attributeNamePrefix: '',
@@ -115,13 +133,15 @@ if (validObj !== true) {
 
 const roadworks = parser.parse(xmlData, parserOptions);
 
-app.get('/', (req, res, next) => {
-  // res.status(200).json(roadworks);
-  res.status(200).json(flattenRoadworks(roadworks));
-});
+if (serving) {
+  app.get('/', (req, res, next) => {
+    // res.status(200).json(roadworks);
+    res.status(200).json(flattenRoadworks(roadworks));
+  });
 
-// app.listen(process.env.POST || 3050, () => {
-//   console.log('Raw JSON server running');
-// });
-
-console.log(JSON.stringify(flattenRoadworks(roadworks), null, 2));
+  app.listen(process.env.POST || 3050, () => {
+    console.log('Raw JSON server running on port 3050');
+  });
+} else {
+  console.log(JSON.stringify(flattenRoadworks(roadworks), null, 2));
+}
