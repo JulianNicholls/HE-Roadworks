48ce6eea415e0a686c5d8cd129e352cd32799ad6
Julian Nicholls <juliannicholls29@gmail.com>
2019-05-01 21:56:10 +0100

    Add links to the header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,26 @@ const Header = () => {
   return (
     <header className="header">
       <h1>Roadworks</h1>
+      <nav>
+        <a href="https://reallybigshoe.co.uk">Home</a>
+        <a href="https://reallybigshoe.co.uk/visualiser">Palette Visualiser</a>
+        <a href="http://spacex.reallybigshoe.co.uk">SpaceX Info</a>
+        <a href="http://countdown.reallybigshoe.co.uk">Countdown Magician</a>
+        <a
+          href="https://github.com/JulianNicholls"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://twitter.com/ReallyBigShoeUK"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+      </nav>
     </header>
   );
 };
