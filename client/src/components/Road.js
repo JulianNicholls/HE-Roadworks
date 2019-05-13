e40fe27e83682d54ff20c01ad62c8373cb3e6648
Julian Nicholls <juliannicholls29@gmail.com>
2019-05-13 10:38:14 +0100

    Add prop-types to Road and basic BGS load.

--- client/src/components/Road.js
+++ client/src/components/Road.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment';
+
+import { notStartedYet, finished } from '../dateRanges';
 
 moment.locale('en-gb');
 
 const dateFormat = 'D MMM YYYY';
+const BGSSite =
+  'https://www.bgs.ac.uk/data/webservices/CoordConvert_LL_BNG.cfc?method=BNGtoLatLng&easting=';
 
 const Road = ({ item }) => {
   const {
@@ -17,7 +22,20 @@ const Road = ({ item }) => {
     centreNorthing,
   } = item;
 
+  const openMap = async (east, north) => {
+    const response = await fetch(`${BGSSite}${east}&northing=${north}`, {
+      mode: 'no-cors',
+    });
+    const data = await response.json();
+
+    const { LONGITUDE, LATITUDE } = data;
+    console.log({ east, north, LONGITUDE, LATITUDE });
+  };
+
   const roadClass = roads[0] === 'A' ? 'a-road' : 'motorway';
+  const shouldBeDone = finished(endDate);
+  const notStarted = notStartedYet(startDate);
+
   const sdate = moment(startDate);
   const edate = moment(endDate);
 
@@ -29,14 +47,23 @@ const Road = ({ item }) => {
         <span>{closureType}</span>
       </header>
       <p>
-        Location: {centreEasting} {centreNorthing}
+        Location: {centreEasting} {centreNorthing}{' '}
+        <button onClick={() => openMap(centreEasting, centreNorthing)}>
+          Map
+        </button>
       </p>
       <p>{description}</p>
       <span className="duration">
-        Duration: {sdate.format(dateFormat)} - {edate.format(dateFormat)}
+        Duration: {sdate.format(dateFormat)} - {edate.format(dateFormat)}{' '}
+        {shouldBeDone && <i>(should be complete)</i>}
+        {notStarted && <i>(Starting soon)</i>}
       </span>
     </article>
   );
+};
+
+Road.propTypes = {
+  item: PropTypes.object.isRequired,
 };
 
 export default Road;
