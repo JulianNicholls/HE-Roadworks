d82fe88adbb5427c966155407a08b27d99189b76
Julian Nicholls <juliannicholls29@gmail.com>
2019-06-01 21:33:28 +0100

    Add message when all roadworks are filtered out

--- client/src/components/RoadworksList.js
+++ client/src/components/RoadworksList.js
@@ -1,16 +1,21 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
-import { WorksContext } from '../context';
+import WorksContext from '../context';
 
 import Road from './Road';
 
 const RoadworksList = ({ setMapCentre, setMapOpen }) => {
   const { selectedRoadworks } = useContext(WorksContext);
 
+  const roadworks = selectedRoadworks();
+
   return (
     <section>
-      {selectedRoadworks().map((item, index) => (
+      {roadworks.length === 0 && (
+        <h2 className="centred">No matching roadworks</h2>
+      )}
+      {roadworks.map((item, index) => (
         <Road
           item={item}
           key={index}
