f862316da256abddfed608a92ba90ef769ea3e25
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-28 15:44:33 +0000

    read-page.js now reads the datafile page.
    Added some more error handling

--- read-page.js
+++ read-page.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
-const { parse } = require('node-html-parser');
+const { parse: parseHTML } = require('node-html-parser');
 const axios = require('axios');
 
 const { validate, parseRoadworks } = require('./load-xml');
+
+const DATAFILE_PAGE =
+  'https://data.gov.uk/dataset/highways_agency_planned_roadworks';
 
 const loadPage = async href => {
   const response = await axios.get(href);
@@ -11,21 +14,23 @@ const loadPage = async href => {
 };
 
 const main = async () => {
-  const wholePage = fs.readFileSync('./data-page.html', 'utf-8');
+  const wholePage = await loadPage(DATAFILE_PAGE);
 
-  const root = parse(wholePage);
+  const root = parseHTML(wholePage);
   const dataFileTRs = root.querySelectorAll('tr.dgu-datafile');
   const liveTRs = dataFileTRs.filter(
     tr => !tr.classNames.includes('js-show-more-datafiles')
   );
 
-  // console.log(`Found ${dataFileTRs.length} datafile <tr>s`);
-  // console.log(`Found ${liveTRs.length} live datafile <tr>s`);
+  if (dataFileTRs.length === 0 || liveTRs.length === 0) {
+    conseole.error('No data file records found at', DATAFILE_PAGE);
+    process.exit(-1);
+  }
 
   const dataFiles = liveTRs.reduce((acc, tr) => {
     const links = tr.querySelectorAll('a');
-    if (links.length > 1) {
-      console.warn({ links });
+    if (links.length !== 1) {
+      console.error('Incorrect Links:', { links });
     } else {
       const href = links[0].attributes.href;
       const matchDate = href.match(/(\d{4})_(\d{2})_(\d{2})\.xml$/);
@@ -39,9 +44,8 @@ const main = async () => {
 
   if (dataFiles[0].publishedDate < dataFiles[1].publishedDate) {
     console.error('Dates are not decreasing');
+    process.exit(-1);
   }
-
-  // console.log({ dataFiles });
 
   const xmlData = await loadPage(dataFiles[0].href);
   const validObj = validate(xmlData);
