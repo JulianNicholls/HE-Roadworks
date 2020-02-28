10642b565c4d4c7826763d6b38e6cd1146092c71
Julian Nicholls <juliannicholls29@gmail.com>
2020-02-28 21:39:09 +0000

    More types

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,10 +1,32 @@
-interface Road {
+interface WorksItem {
   roads: string;
   description: string;
   startDate: Date;
   endDate: Date;
   expectedDelay: string;
   closureType: string;
-  centreEasting: string;
-  centreNorthing: string;
+  centreEasting: number;
+  centreNorthing: number;
+}
+
+interface ENLocation {
+  east: number;
+  north: number;
+}
+
+interface RoadIndex {
+  roads: string;
+  index: number;
+}
+
+interface WorksState {
+  roadworks: Array<WorksItem>;
+  roads: Array<RoadIndex>;
+  selected: string;
+  searchText: string;
+  centreEN: ENLocation;
+  selectedRoadworks(): Array<WorksItem>;
+  setSelected(string): void;
+  setSearchText(string): void;
+  setCentreEN(ENLocation): void;
 }
