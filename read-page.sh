04b734b52ac49bbed16fbdb6e5fd33344e02a3df
Julian Nicholls <juliannicholls29@gmail.com>
2020-07-27 21:02:32 +0100

    read-page now updates online roadworks

--- read-page.sh
+++ read-page.sh
@@ -2,4 +2,11 @@
 
 # cd /home/julian/sources/react/roadworks
 DATE=`date +%Y-%m-%d`
-ts-node read-page.ts >"roadworks-$DATE.json"
+# ts-node read-page.ts >"roadworks-$DATE.json"
+cp "roadworks-$DATE.json" ~/sources/serverless/roadworks.json
+
+read -p "Update roadworks online? [Y/n] " go
+if [ -z "$go" -o "$go" = "y" -o "$go" = "Y" ]; then
+  cd ~/sources/serverless
+  vercel --prod
+fi
